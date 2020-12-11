ecce314943b0457333283efa7080ac32c80049ab
Janelle <janelle.wen@gmail.com>
2020-12-11 17:38:20 -0500

    Adding more animation / js

---
+++ homework_8/Assets/main.js
@@ -0,0 +1,52 @@
+// Scroll Indicator
+
+window.onscroll = function () { myFunction() };
+
+function myFunction() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+}
+
+// Scroll Reveal
+ScrollReveal().reveal('div', {delay: 100, reset: false, duration: 600, easing: 'cubic-bezier(0.2, 0, 0, 1)',
+});
+
+
+// Typed JS
+$(function(){
+	$(".typed").typed({
+		strings: ["Product Designer", "Problem Solver", "People Person"],
+		// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+		stringsElement: null,
+		// typing speed
+		typeSpeed: 30,
+		// time before typing starts
+		startDelay: 1200,
+		// backspacing speed
+		backSpeed: 20,
+		// time before backspacing
+		backDelay: 500,
+		// loop
+		loop: true,
+		// false = infinite
+		loopCount: 5,
+		// show cursor
+		showCursor: false,
+		// character for cursor
+		cursorChar: "|",
+		// attribute to type (null == text)
+		attr: null,
+		// either html or text
+		contentType: 'html',
+		// call when done callback function
+		callback: function() {},
+		// starting callback function before each string
+		preStringTyped: function() {},
+		//callback for every typed string
+		onStringTyped: function() {},
+		// callback for reset
+		resetCallback: function() {}
+	});
+});

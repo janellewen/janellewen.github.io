a31eed35cc3294051ca62e716c3c6ba245f44bef
Janelle <janelle.wen@gmail.com>
2020-12-11 20:01:41 -0500

    added typed.js

--- homework_8/Assets/main.js
+++ homework_8/Assets/main.js
@@ -17,36 +17,21 @@ ScrollReveal().reveal('div', {delay: 100, reset: false, duration: 600, easing: '
 // Typed JS
 $(function(){
 	$(".typed").typed({
-		strings: ["Product Designer", "Problem Solver", "People Person"],
-		// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+		strings: ["Problem Solver.", "People Person.", "Designer."],
 		stringsElement: null,
-		// typing speed
-		typeSpeed: 30,
-		// time before typing starts
-		startDelay: 1200,
-		// backspacing speed
-		backSpeed: 20,
-		// time before backspacing
-		backDelay: 500,
-		// loop
+		typeSpeed: 100,
+		startDelay: 1500,
+		backSpeed: 50,
+		backDelay: 800,
 		loop: true,
-		// false = infinite
 		loopCount: 5,
-		// show cursor
 		showCursor: false,
-		// character for cursor
 		cursorChar: "|",
-		// attribute to type (null == text)
 		attr: null,
-		// either html or text
 		contentType: 'html',
-		// call when done callback function
 		callback: function() {},
-		// starting callback function before each string
 		preStringTyped: function() {},
-		//callback for every typed string
 		onStringTyped: function() {},
-		// callback for reset
 		resetCallback: function() {}
 	});
 });
